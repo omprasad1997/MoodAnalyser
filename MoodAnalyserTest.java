345501a3d6295a82b101e533863400f6b3672cfd
Omprasad Rathod <20rathodomprasad@gmail.com>
2021-02-21 18:21:01 +0530

    Mood Analyser Test file

---
+++ MoodAnalyserTest.java
@@ -0,0 +1,20 @@
+package com.moodanalyser;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class MoodAnalyserTest {
+    @Test
+    public void givenMessage_WhenSad_ShouldReturnSad() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is Sad Message");
+        Assertions.assertEquals("SAD",mood);
+    }
+
+    @Test
+    public void givenMessage_WhenNotSad_ShouldReturnHappy() {
+        MoodAnalyser moodAnalyser = new MoodAnalyser();
+        String mood = moodAnalyser.analyseMood("This is Happy Message");
+        Assertions.assertEquals("Happy",mood);
+    }
+}
